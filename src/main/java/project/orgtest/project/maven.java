8e91304a8aaaa5ce72e139f12e5fd1322ff3944c
Revati12 <revatirevz12@gmail.com>
2021-05-07 19:33:32 +0530

    I want to push the code to master

---
+++ src/main/java/project/orgtest/project/maven.java
@@ -0,0 +1,15 @@
+package project.orgtest.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class maven {
+
+    public static void main( String[] args )
+    {
+    	int a=0;
+    
+        System.out.println( "a" );
+    }
+}
